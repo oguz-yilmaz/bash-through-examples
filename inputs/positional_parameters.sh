281cc2c193bd8b15a02bcba40fc341be30d10e2a
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-10-29 17:32:01 +0300

    Positional parameters added

---
+++ inputs/positional_parameters.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Shell assigns numbers to each cli argument
+
+test() {
+    echo "First argument is : $1"
+    echo "Second argument is : $2"
+    echo "Third argument is : $3"
+}
+
+test first second third
