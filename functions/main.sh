b127fbd23fc631846ee1d3981a25820b200acfd0
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-10-30 01:01:40 +0300

    Basic function definition added

---
+++ functions/main.sh
@@ -0,0 +1,29 @@
+sayHello() {
+	echo "Hello there"
+	echo "The time is $(date)"
+	echo "Have a good day"
+}
+
+echo "Running the function sayHello"
+sayHello
+
+sayHelloWithParameters() {
+	echo "Hello there $1"
+	echo "Hello there $2"
+	return 0
+}
+
+echo "Running the function sayHelloWithParameters"
+sayHelloWithParameters Oguz Eda
+
+funcThatFails() {
+	echo "Failed func since it returned non-zero exit code"
+  return 35
+}
+
+if [ funcThatFails ]; then
+	echo "test failed"
+else
+  #outputs this
+  echo "test passed"
+fi
