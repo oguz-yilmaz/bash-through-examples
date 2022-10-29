2da16a7106acfe453cbbd916748ce8c4a1b7a05b
Oguz Yilmaz <oguz.yilmaz@yahoo.com>
2022-10-29 17:35:13 +0300

    Special parameters added

--- inputs/special_parameters.sh
+++ inputs/special_parameters.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Shell assigns numbers to each cli argument
+# $0 name of the script
+# $# number of the argument
 
 test_command() {
+    echo "The command $0 has:"
     echo "First argument is : $1"
     echo "Second argument is : $2"
     echo "Third argument is : $3"
+    echo "Number of aregument: $#"
 }
 
 test_command first second third
